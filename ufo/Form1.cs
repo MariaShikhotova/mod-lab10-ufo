483413fca2d195a06436fa029eed2f4b4bf03bab
MariaShikhotova <126818543+MariaShikhotova@users.noreply.github.com>
2023-05-31 18:32:43 +0300

    Update Form1.cs

--- ufo/Form1.cs
+++ ufo/Form1.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
-namespace лаб_10
+namespace ufo
 {
     public partial class Form1 : Form
     {
